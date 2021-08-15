2b5dfad55b8e43f45dbf0edc6fb05b3dbb74ea66
Rushil Patel <softcosol@gmail.com>
2021-08-15 18:18:05 -0400

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.typed', {
+    strings: ["", "Let us enhance your business agility and efficiency with cutting-edge software solutions!"],
+    typeSpeed: 30,
+    showCursor: false
+});
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-177859514-1');
